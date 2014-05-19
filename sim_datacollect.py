df194f1bb0e6d9e7c8d834cfae75badc72d20b14
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2014-05-19 11:47:05 -0400

    simulation routine

--- sim_datacollect.py
+++ sim_datacollect.py
@@ -3,8 +3,8 @@ __author__ = 'hok1'
 import sim_mr_canonical as sim
 import csv
 
-N = 100000000
-total_energies = [10, 50, 100, 500, 1000, 5000, 10000, 25000, 50000, 100000, 250000, 500000, 1000000]
+N = 10000000
+total_energies = [10, 50, 100, 500, 1000, 5000, 10000, 25000, 50000, 100000]
 num_workers = 10
 nrep = 20
 filename = 'raw_canonical_sim.csv'
@@ -16,7 +16,9 @@ if __name__ == '__main__':
     header = ['N', 'total_energy', 'mean_energy', 'std_energy', 'beta', 'alpha']
     writer.writerow(header)
     for total_energy in total_energies:
+        print "Total Energy = ", total_energy
         for i in range(nrep):
+            print "\tRepeat ", i
             occupancies = simulator.simulate_multithread(N, total_energy, numthreads=num_workers)
             coefs, mean_energy, std_energy = simulator.stat_analyze(occupancies)
             writer.writerow([N, total_energy, mean_energy, std_energy, -coefs[0], coefs[1]])
